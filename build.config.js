1fdf6e41da80bd1437a5f2f72c0cd5f3b206cb08
Master Yoda <deebeat@students.uonbi.ac.ke>
2015-10-24 22:46:12 +0300

    Finalize on Scafolding, resolves #1

--- build.config.js
+++ build.config.js
@@ -71,7 +71,16 @@ module.exports = {
         js: [
             "vendor/jquery/dist/jquery.js",
             "vendor/angular/angular.js",
-            "vendor/underscore/underscore.js"
+            "vendor/underscore/underscore.js",
+            "vendor/bootstrap/dist/js/bootstrap.js",
+            "vendor/angular-resource/angular-resource.js",
+            "vendor/angular-animate/angular-animate.js",
+            "vendor/angular-ui-router/release/angular-ui-router.js",
+            "vendor/angular-bootstrap/ui-bootstrap.js",
+            "vendor/angular-bootstrap/ui-bootstrap-tpls.js",
+            "vendor/js-data/dist/js-data.js",
+            "vendor/js-data-angular/dist/js-data-angular.js",
+            "vendor/js-data-schema/dist/js-data-schema.js"
         ],
         css: [
         ],
